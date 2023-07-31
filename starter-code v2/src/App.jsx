eb130be2e4f19a3176ead24d8c3b7e07a0617f99
di-wee <Dionis.w96@gmail.com>
2023-07-31 13:21:41 +0800

    nav bar created

--- starter-code v2/src/App.jsx
+++ starter-code v2/src/App.jsx
@@ -2,6 +2,20 @@ import React, { useState } from 'react';
 import Display from './components/Display';
 import List from './components/List';
 import RestaurantContext from './RestaurantContext';
+import NavBar from './components/NavBar';
+import { createTheme, ThemeProvider, CssBaseline, Box } from '@mui/material';
+import { grey } from '@mui/material/colors';
+import '@fontsource/roboto-condensed';
+
+//creating dark theme
+const darkerTheme = createTheme({
+	palette: {
+		primary: grey,
+	},
+	typography: {
+		fontFamily: 'Roboto Condensed',
+	},
+});
 
 function App() {
 	const [restaurant, setRestaurant] = useState([]);
@@ -10,23 +24,26 @@ function App() {
 	const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
 	return (
-		<div>
-			<RestaurantContext.Provider
-				value={{
-					restaurant,
-					setRestaurant,
-					showList,
-					setShowList,
-					filteredRestaurant,
-					setFilteredRestaurant,
-					cafe,
-					setCafe,
-				}}
-			>
-				<Display></Display>
-				{showList && <List></List>}
-			</RestaurantContext.Provider>
-		</div>
+		<ThemeProvider theme={darkerTheme}>
+			<div>
+				<RestaurantContext.Provider
+					value={{
+						restaurant,
+						setRestaurant,
+						showList,
+						setShowList,
+						filteredRestaurant,
+						setFilteredRestaurant,
+						cafe,
+						setCafe,
+					}}
+				>
+					<NavBar></NavBar>
+					<Display></Display>
+					{showList && <List></List>}
+				</RestaurantContext.Provider>
+			</div>
+		</ThemeProvider>
 	);
 }
 
